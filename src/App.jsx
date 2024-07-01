f8be8c994d79a81582f8b9621f642f3599884a6a
Patrickbdn <patrick.bodin.pro@gmail.com>
2024-07-01 10:19:17 +0200

    Sauvegarde

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import React, { useState } from 'react';
-import Counter from './Components/Counter/Counter';
-import Button from './Components/Button/Button';
+import React, { useState } from 'react';// pour charger la bibliothèque React
+import Counter from './Components/Counter/Counter';// pour charger le jsx counter
+import Button from './Components/Button/Button';// pour charger le jsx button
+import './App.css';
 
 const App = () => {
   const [count, setCount] = useState(0);
 
-  const increment = () => setCount(count + 1);
+  const increment = () => setCount(count + 1);//pour incrementer de 1 le compteur
   const decrement = () => {
     if (count > 0) {setCount(count - 1);
-    }
+    }//pour décrementer le compteur de 1 mais pas sous 0
   };
-  const reset = () => setCount(0);
+  const reset = () => setCount(0);// pour remettre le compteur à 0
 
   return (
     <div className="App">
       <h1>Compteur de Participants</h1>
       <Counter count={count} />
       <div>
-        <Button onClick={increment} label="Incrémenter" />
+        <Button onClick={increment} label="Incrémenter" /> 
         <Button onClick={decrement} label="Décrémenter" />
         <Button onClick={reset} label="Réinitialiser" />
       </div>
